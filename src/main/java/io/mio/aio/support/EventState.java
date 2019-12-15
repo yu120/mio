86a4aef1edd86189efdbf329774b518a87a1f43c
lry <595208882@qq.com>
2019-12-15 10:12:22 +0800

    init

--- src/main/java/io/mio/aio/support/EventState.java
+++ src/main/java/io/mio/aio/support/EventState.java
@@ -3,6 +3,7 @@ package io.mio.aio.support;
 import io.mio.aio.MessageProcessor;
 import io.mio.aio.Protocol;
 
+import java.nio.channels.CompletionHandler;
 import java.nio.ByteBuffer;
 
 /**
@@ -20,52 +21,53 @@ public enum EventState {
      * 读通道已被关闭。
      * <p>
      * 通常由以下几种情况会触发该状态：
-     * <ol>
-     * <li>对端主动关闭write通道，致使本通常满足了EOF条件</li>
-     * <li>当前AioMioSession处理完读操作后检测到自身正处于{@link EventState#SESSION_CLOSING}状态</li>
-     * </ol>
-     * </p>
-     * <b>未来该状态机可能会废除，并转移至NetMonitor</b>
+     * 1.对端主动关闭write通道，致使本通常满足了EOF条件
+     * 2.当前AioMioSession处理完读操作后检测到自身正处于{@link EventState#SESSION_CLOSING}状态
+     * <p>
+     * 未来该状态机可能会废除，并转移至NetMonitor
      */
     INPUT_SHUTDOWN,
     /**
      * 业务处理异常。
-     * <p>执行{@link MessageProcessor#process(AioMioSession, Object)}期间发生用户未捕获的异常。</p>
+     * <p>
+     * 执行{@link MessageProcessor#process(AioMioSession, Object)}期间发生用户未捕获的异常。
      */
     PROCESS_EXCEPTION,
 
     /**
      * 协议解码异常。
-     * <p>执行{@link Protocol#decode(ByteBuffer, AioMioSession)}期间发生未捕获的异常。</p>
+     * <p>
+     * 执行{@link Protocol#decode(ByteBuffer, AioMioSession)}期间发生未捕获的异常。
      */
     DECODE_EXCEPTION,
     /**
      * 读操作异常。
-     *
-     * <p>在底层服务执行read操作期间因发生异常情况出发了{@link java.nio.channels.CompletionHandler#failed(Throwable, Object)}。</p>
-     * <b>未来该状态机可能会废除，并转移至NetMonitor</b>
+     * <p>
+     * 在底层服务执行read操作期间因发生异常情况出发了{@link CompletionHandler#failed(Throwable, Object)}。
+     * <p>
+     * 未来该状态机可能会废除，并转移至NetMonitor
      */
     INPUT_EXCEPTION,
     /**
      * 写操作异常。
-     * <p>在底层服务执行write操作期间因发生异常情况出发了{@link java.nio.channels.CompletionHandler#failed(Throwable, Object)}。</p>
-     * <b>未来该状态机可能会废除，并转移至NetMonitor</b>
+     * <p>
+     * 在底层服务执行write操作期间因发生异常情况出发了{@link CompletionHandler#failed(Throwable, Object)}。
+     * <p>
+     * 未来该状态机可能会废除，并转移至NetMonitor
      */
     OUTPUT_EXCEPTION,
     /**
-     * 会话正在关闭中。
-     *
-     * <p>执行了{@link AioMioSession#close(boolean false)}方法，并且当前还存在待输出的数据。</p>
+     * 会话正在关闭中
+     * <p>
+     * 执行了{@link AioMioSession#close(boolean false)}方法，并且当前还存在待输出的数据。
      */
     SESSION_CLOSING,
     /**
-     * 会话关闭成功。
-     *
-     * <p>AioMioSession关闭成功</p>
+     * 会话关闭成功
      */
     SESSION_CLOSED,
     /**
-     * 拒绝接受连接,仅Server端有效
+     * 拒绝接受连接(Server端)
      */
     REJECT_ACCEPT
 
